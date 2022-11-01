177196081c708e15504614a9bf23257834a97f3f
Nicklaw42 <89273070+Nicklaw42@users.noreply.github.com>
2022-10-31 21:11:09 -0500

    Added First Page
    Base page of website.

---
+++ Practice Webpage/js/app.js
@@ -0,0 +1,25 @@
+$("#btnHide").click(function(){
+    $("<h1 class='btn-warning align-self-center'>Hi!</h1>").clone().appendTo("#mainFrame");
+});
+
+$("#btnSpanish").click(function(){
+    $("#crdHello").text("El Sitio Linux de Nick!")
+    $("#btnHide").text("Hace un buton nuevo!")
+    $("#btnEnglish").css({"visibility": "visible"})
+    $("#btnSpanish").css({"visibility": "Hidden"})
+
+    //look into ordinality with CSS, maybe stick to CSS docks rather than html
+});
+$("#btnEnglish").click(function(){
+    $("#crdHello").text("Nick's Linux Site!")
+    $("#btnHide").text("Make a New Button!")
+    $("#btnSpanish").css({"visibility": "visible"})
+    $("#btnEnglish").css({"visibility": "Hidden"})
+});
+
+
+var spanish = false;
+
+$("#btnSpanish").click(function(){
+    $("#crdHello").on("")
+})
