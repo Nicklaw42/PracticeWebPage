407c8fb227da5e538bc4260c534fd5bc7a30a394
Nicklaw42 <89273070+Nicklaw42@users.noreply.github.com>
2022-11-29 11:16:16 -0600

    Fiddled with stuff
    Messed to try to style datatable. Added some entries too.

--- Practice Webpage/js/app.js
+++ Practice Webpage/js/app.js
@@ -1,3 +1,4 @@
+
 window.onscroll = function() {myFunction()};
 
 // Get the header
@@ -14,6 +15,7 @@ function myFunction() {
     header.classList.remove("sticky");
   }
 } 
+
 
 
 $(document).ready(function(){
